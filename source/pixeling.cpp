7516bd2cbb5d2e15e08f5e4d715faf349482ccb8
va65kef <philip.fuerste@uni-jena.de>
2020-02-21 20:16:22 +0100

    added right & bottom overhead for pixelation

--- source/pixeling.cpp
+++ source/pixeling.cpp
@@ -1,4 +1,4 @@
-#include <opencv2/opencv.hpp>
+#include<opencv2/opencv.hpp>
 #include<iostream>
 #include<vector>
 
@@ -15,16 +15,24 @@ void zeroPad(cv::Mat src, cv::Mat dst, int padding){
 	cv::imwrite("../io/paddedimg.png", dst);
 }
 
-// TODO: parallelize & overhead
+// TODO: parallelize
 void pixelateByAvg(cv::Mat img, cv::Mat dst, int regionSize){
 	int rowSteps = img.rows/regionSize;
 	int colSteps = img.cols/regionSize;
-	for (int i=0; i < rowSteps; i++){
-		for (int j=0; j < colSteps; j++){
+	int rowOverhead = img.rows%regionSize;
+	int colOverhead = img.cols%regionSize;
+	std::cout<<rowOverhead<<std::endl;
+	int regionRows, regionCols;
+	for (int i=0; i <= rowSteps; i++){
+		for (int j=0; j <= colSteps; j++){
+			if (i == rowSteps & rowOverhead != 0) regionRows = rowOverhead;
+			else regionRows = regionSize;
+			if (j == colSteps & colOverhead != 0) regionCols = colOverhead;
+			else regionCols = regionSize;
 			cv::Vec3b pixelVals;
 			std::vector<int> pixelAvg(3);
-			for (int m=0; m < regionSize; m++){
-				for (int n=0; n < regionSize; n++){
+			for (int m=0; m < regionRows; m++){
+				for (int n=0; n < regionCols; n++){
 					pixelVals = img.at<cv::Vec3b>(i*regionSize+m, j*regionSize+n);
 					for (int k=0; k < pixelAvg.size(); k++){
 						pixelAvg[k] += (int)pixelVals[k];
@@ -32,18 +40,16 @@ void pixelateByAvg(cv::Mat img, cv::Mat dst, int regionSize){
 				}
 			}
 			for (int k=0; k < pixelAvg.size(); k++){
-				pixelAvg[k] = pixelAvg[k]/(regionSize*regionSize);
+				pixelAvg[k] = pixelAvg[k]/(regionRows*regionCols);
 			}
 			cv::Vec3b pixelAvg3b(pixelAvg[0], pixelAvg[1], pixelAvg[2]);
-			for (int m=0; m < regionSize; m++){
-				for (int n=0; n < regionSize; n++){
+			for (int m=0; m < regionRows; m++){
+				for (int n=0; n < regionCols; n++){
 					dst.at<cv::Vec3b>(i*regionSize+m, j*regionSize+n) = pixelAvg3b;
 				}
 			}
 		}
 	}
-	//std::cout<<  <<std::endl;
-	//cv::imwrite("../io/test1Pixelated.png", dst);
 }
 
 cv::Mat pixelate(cv::Mat img, std::string processor, std::string args){
@@ -53,20 +59,3 @@ cv::Mat pixelate(cv::Mat img, std::string processor, std::string args){
 	}
 	return dst;
 }
-/* int main(int argc, char const *argv[]){
-	int padding = 10;
-	int regionSize = padding*2+1;
-	cv::Mat img = cv::imread("../io/test1.jpg");
-	//std::cout<< image.rows << image.cols <<std::endl;
-	//cv::Mat src = cv::Mat::zeros(cv::Size(image.cols+2*padding, image.rows+2*padding), CV_8UC3);
-	//std::cout<< src.rows << src.cols<< std::endl;
-	//zeroPad(image, src, padding);
-	
-	cv::Mat src = img.clone();
-	cv::Mat dst = src.clone();
-	//std::cout<< src.rows << src.cols<< std::endl;
-	std::cout<< "cloned" <<std::endl;
-	pixelateByRegions(src, dst, padding, regionSize);
-	return 0;
-}
- */
